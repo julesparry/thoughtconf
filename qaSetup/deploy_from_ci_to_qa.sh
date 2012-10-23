2c44d0d210875e2b589afac14ee539d6b05fbb71
mwangm <mwang@thoughtworks.com>
2012-10-23 17:30:26 +0530

    correct the script for qa machine

--- qaSetup/deploy_from_ci_to_qa.sh
+++ qaSetup/deploy_from_ci_to_qa.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#! /bin/bash
 
 #path to pull war from ci machine
-sourcFilePath = /var/lib/go-agent/pipelines/CI/build/dist/app.war
+sourceFilePath=/var/lib/go-agent/pipelines/CI/build/dist/app.war
 
 #user on qa machine
-user = root
+user=root
 
 #qa machine ip
-QA_ip = 10.10.4.124
+QA_ip=10.10.4.124
 
 #path to deploy war to on qa machine
-deployPath =/root/Test/ 
+deployPath=/root/Test/ 
 
 
-scp $sourcePath $user"@"QA_ip":/"$deployPath 
+scp $sourceFilePath $user"@"QA_ip":/"$deployPath 
