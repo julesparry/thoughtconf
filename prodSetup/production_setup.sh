9a89429d967dff870192c2c391011d589f736fd5
lwyglend <lwyglend@thoughtworks.com>
2012-10-23 17:04:20 +0530

    laura & yaxuan: modified production_setup script to use locally networked installation files, and sudo for jdk installation with rpm.

--- prodSetup/production_setup.sh
+++ prodSetup/production_setup.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 echo "Running inside our script"
-rm -f workspace:
+
 mkdir workspace
 cd workspace
 
-wget https://dl.dropbox.com/u/56567206/jdk-7u9-linux-x64.rpm 
-rpm -iv jdk-7u9-linux-x64.rpm 
+wget http://10.10.4.124:8080/jdk-7u9-linux-x64.rpm 
+sudo rpm -iv jdk-7u9-linux-x64.rpm 
 
-wget http://apache.techartifact.com/mirror/tomcat/tomcat-7/v7.0.32/bin/apache-tomcat-7.0.32.tar.gz 
+wget http://10.10.4.124:8080/apache-tomcat-7.0.32.tar.gz 
 tar -xvzf apache-tomcat-7.0.32.tar.gz 
 
 ./apache-tomcat-7.0.32/bin/startup.sh
