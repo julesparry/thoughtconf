6a2ecd4eccfec85d98ae47021704fa33490dea53
ntetteh <niiashikwei@hotmail.com>
2012-10-24 15:45:24 +0530

    adding slash to path to fix ci deployment error

--- qaSetup/deploy_from_ci_to_qa.sh
+++ qaSetup/deploy_from_ci_to_qa.sh
@@ -16,7 +16,7 @@ user=root
 QA_ip="10.10.4.126"
 
 #path to deploy war to on qa machine
-deployPath=/home/thoughtconf/workspace/apache-tomcat-7.0.32/webapps/ROOT 
+deployPath=/home/thoughtconf/workspace/apache-tomcat-7.0.32/webapps/ROOT/ 
 
 
 #command to deploay war file from war path on CI to correct path on qa (temp path currently used)
