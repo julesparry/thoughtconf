ba2f129746dc60d98ee378f1f62242ad994b399a
ntetteh <niiashikwei@hotmail.com>
2012-10-24 15:33:10 +0530

    corrected qa deployment script to use updated path to proper qa server

--- qaSetup/deploy_from_ci_to_qa.sh
+++ qaSetup/deploy_from_ci_to_qa.sh
@@ -13,10 +13,10 @@ sourceFilePath=/var/lib/go-agent/pipelines/CI/build/dist/app.war
 user=root
 
 #qa machine ip
-QA_ip="10.10.4.124"
+QA_ip="10.10.4.126"
 
 #path to deploy war to on qa machine
-deployPath=/root/Test/ 
+deployPath=/home/thoughtconf/workspace/apache-tomcat-7.0.32/webapps/ROOT 
 
 
 #command to deploay war file from war path on CI to correct path on qa (temp path currently used)
