eb8a884c55f7c248967cce86d8a558165995cf24
mwangm <mwang@thoughtworks.com>
2012-10-25 16:59:59 +0530

    add commons-dbpc when runtime in ivy

--- src/main/java/com/example/app/web/controller/ExampleController.java
+++ src/main/java/com/example/app/web/controller/ExampleController.java
@@ -1,7 +1,6 @@
 package com.example.app.web.controller;
 
 import com.example.app.services.ExampleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +14,6 @@ public class ExampleController {
 
     private ExampleService exampleService;
 
-    @Autowired
     public ExampleController(ExampleService exampleService) {
         this.exampleService = exampleService;
     }
