f1567b77a52e8631fa2f879c30dae1cc51fe0844
mwangm <mwang@thoughtworks.com>
2012-10-25 16:06:56 +0530

    rename class and change dependecy inof slf4j

--- src/main/java/com/example/app/web/controller/HomePageController.java
+++ src/main/java/com/example/app/web/controller/HomePageController.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.example.app.services.FooService;
+import com.example.app.services.UserService;
 
 @Controller
 public class HomePageController {
     
     @Autowired
-    private FooService fooService ;
+    private UserService userService;
 
     @RequestMapping("/home")
     public String index(ModelMap modelMap) {
