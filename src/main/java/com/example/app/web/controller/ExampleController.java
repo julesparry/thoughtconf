f929f2577f824087147eebb00446fb345d27e703
mwangm <mwang@thoughtworks.com>
2012-10-25 17:12:52 +0530

    Meng Dj Yx: Add the service bean

--- src/main/java/com/example/app/web/controller/ExampleController.java
+++ src/main/java/com/example/app/web/controller/ExampleController.java
@@ -1,6 +1,7 @@
 package com.example.app.web.controller;
 
 import com.example.app.services.ExampleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("something")
 public class ExampleController {
 
+    @Autowired
     private ExampleService exampleService;
 
     public ExampleController(ExampleService exampleService) {
