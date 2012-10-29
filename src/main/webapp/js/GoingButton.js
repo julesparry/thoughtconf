1015d3ae7cc7e91a3623cd6c6349a5d9ff7043a9
Akriti Ayushi <akritiay@Akritiay.corporate.thoughtworks.com>
2012-10-29 10:25:35 +0530

    removed example files from jasmine

--- src/main/webapp/js/GoingButton.js
+++ src/main/webapp/js/GoingButton.js
@@ -3,7 +3,7 @@ function initGoingButton() {
     $(".going_button").addClass("going_button_inactive");
     going = false;
     $(".going_button_inactive").click(toggleGoing);
-    //$(".going_button_active").click(toggleNotGoing);
+
 }
 function toggleGoing() {
     $(this).removeClass("going_button_inactive");
