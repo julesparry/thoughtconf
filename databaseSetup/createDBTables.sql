d3258fef24107f5ef6f198d989a679cfd04d7cfb
ditjhaba <eseleme@thoughtworks.com>
2012-10-30 17:01:44 +0530

    Adding the database script file to the repository.

---
+++ databaseSetup/createDBTables.sql
@@ -0,0 +1,42 @@
+
+USE ThoughtConf;
+
+DROP TABLE IF EXISTS conference;
+
+CREATE TABLE conference(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+conference_name VARCHAR(200),
+conference_date DATETIME,
+conference_location VARCHAR(200),
+Abstract VARCHAR(200),
+conference_type VARCHAR(200));
+
+DROP TABLE IF EXISTS track;
+
+CREATE TABLE track(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+track_name VARCHAR(200),
+track_description VARCHAR(200), 
+conference_id INT);
+
+ALTER TABLE track 
+ADD CONSTRAINT FK_Conference_Track FOREIGN KEY(conference_id) 
+REFERENCES conference(id) ON DELETE CASCADE;
+
+DROP TABLE IF EXISTS session;
+
+CREATE TABLE session 
+(session_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ session_name VARCHAR(200),
+ session_description VARCHAR(200),
+ session_time DATETIME,
+ session_location VARCHAR(200),
+ session_type VARCHAR(200),
+ abstract VARCHAR(200),
+ session_speaker VARCHAR(200),
+ about_speaker VARCHAR(200),
+ track_id INT);
+
+ALTER TABLE session 
+ADD CONSTRAINT FK_Track_Session FOREIGN KEY(track_id)
+REFERENCES track(id) ON DELETE CASCADE; 
