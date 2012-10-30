b165cd7cc431efc39c278fdf1eaf2d5812e92c5d
arathyjan <arathyjanardhanan@gmail.com>
2012-10-30 15:16:02 +0530

    #7 Arathy, Laura, Jules: Added a test and implementation for session controller

--- src/main/java/com/twu/thoughtconf/web/controller/SessionController.java
+++ src/main/java/com/twu/thoughtconf/web/controller/SessionController.java
@@ -3,13 +3,13 @@ package com.twu.thoughtconf.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SessionController {
 
     @RequestMapping("/view-session")
-    public String index(ModelMap modelMap) {
-        return "viewSessionDetail";
+    public ModelAndView display() {
+        return new ModelAndView("viewSessionDetail");
     }
-
 }
