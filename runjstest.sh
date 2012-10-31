2ba04430d63e21ebe85791109620da990e03cafb
ntetteh <niiashikwei@hotmail.com>
2012-10-31 15:47:18 +0530

    Nii & Vidhi modified run-js-tests script to start and stop display and fail on error

--- runjstest.sh
+++ runjstest.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "Setting up Xvfb."
+Xvfb :3 -ac &
+PID_XVFB="$!"
+export DISPLAY=:3
 
- java -jar src/main/webapp/jasmine/jasmine-1.2.0/JsTestDriver-1.3.5.jar  --port 4224 --reset --config src/main/webapp/jasmine/jsTestDriver.conf --browser firefox  --tests all --raiseOnFailure true
+echo "Running Tests"
+java -jar src/main/webapp/jasmine/jasmine-1.2.0/JsTestDriver-1.3.5.jar  --port 4224 --reset --config src/main/webapp/jasmine/jsTestDriver.conf --browser firefox  --tests all --raiseOnFailure true
 
+echo "Killing Xvfb display."
+kill PID_XVFB
