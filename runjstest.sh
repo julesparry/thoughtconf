c699a9571b16b6f30ca883802a90136220d3754e
ntetteh <niiashikwei@hotmail.com>
2012-11-02 09:41:45 +0530

    Nii & Vidhi added if clause to check for CI environment and set up display only if needed

--- runjstest.sh
+++ runjstest.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo "Setting up Xvfb."
-Xvfb :3 -ac &
-export DISPLAY=:3
+
+if [[ -n "$GO_STAGE_NAME"]]
+then
+    echo "We're on the CI!"
+    echo "Setting up Xvfb."
+    Xvfb :3 -ac &
+    export DISPLAY=:3
+fi
+
 
 echo "Running Tests"
 java -jar src/main/webapp/jasmine/jasmine-1.2.0/JsTestDriver-1.3.5.jar  --port 4224 --reset --config src/main/webapp/jasmine/jsTestDriver.conf --browser firefox  --tests all --raiseOnFailure true
