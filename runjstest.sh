063d4a795287a8954b836e3146f6630af4b93036
yxzhang <zhangyaxuan310@gmail.com>
2012-11-06 11:45:00 +0530

    Yanhui & Laura & Yaxuan: changed the runjstest.sh to work with firefox on mac

--- runjstest.sh
+++ runjstest.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+FIREFOX_COMMAND=firefox
+
+if [[ -s /Applications/Firefox.app/Contents/MacOS/firefox-bin ]]
+then
+    FIREFOX_COMMAND=/Applications/Firefox.app/Contents/MacOS/firefox-bin
+fi
+
 if [[ -n "$GO_STAGE_NAME" ]]
 then
     echo "We're on the CI!"
@@ -9,5 +16,5 @@ then
 fi
 
 echo "Running Tests"
-java -jar src/main/webapp/jasmine/jasmine-1.2.0/JsTestDriver-1.3.5.jar  --port 4224 --reset --config src/main/webapp/jasmine/jsTestDriver.conf --browser firefox  --tests all --raiseOnFailure true
+java -jar src/main/webapp/jasmine/jasmine-1.2.0/JsTestDriver-1.3.5.jar  --port 4224 --reset --config src/main/webapp/jasmine/jsTestDriver.conf --browser $FIREFOX_COMMAND  --tests all --raiseOnFailure true
 
