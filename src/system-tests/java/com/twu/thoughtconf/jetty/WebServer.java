a823081b4c42c55e3a8f5a0fd51cf3234f12aac1
niiashikwei <niiashikwei@hotmail.com>
2012-11-15 15:52:28 +0530

    Nii & Nikita & Yanhui - Run Selenium tests based on local Jetty

--- src/system-tests/java/com/twu/thoughtconf/jetty/WebServer.java
+++ src/system-tests/java/com/twu/thoughtconf/jetty/WebServer.java
@@ -1,15 +1,30 @@
 package com.twu.thoughtconf.jetty;
 
 import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.AbstractHandler;
 import org.mortbay.jetty.webapp.WebAppContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class WebServer {
 
+    private static final AbstractHandler SHUTDOWN_HANDLER = new AbstractHandler() {
+        @Override
+        public void handle(String uri, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, int i) throws IOException, ServletException {
+            if ("/shutdown".equals(uri)) {
+                System.exit(0);
+            }
+        }
+    };
     private final Server jetty;
 
     public WebServer(int port) {
         jetty = new Server(port);
         jetty.addHandler(new WebAppContext("src/main/webapp", "/thoughtconf"));
+        jetty.addHandler(SHUTDOWN_HANDLER);
     }
 
     public WebServer start() throws Exception {
